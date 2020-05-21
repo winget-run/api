269501fde2e5da1612b59200642db91c3f01436f
Lukasz Niezabitowski <lukasznie1320@gmail.com>
2020-05-21 12:53:31 +0100

    i added the github secrets to the wrong repo...

--- src/server/buildFastify.ts
+++ src/server/buildFastify.ts
@@ -4,7 +4,7 @@ const buildFastify = (settings = {}): FastifyInstance => {
   const fastify = Fastify(settings);
 
   fastify.get("/", async () => ({
-    nonce: "mattheousfans",
+    nonce: "me, i added the github secrets to the wrong repo lol",
   }));
 
   return fastify;
