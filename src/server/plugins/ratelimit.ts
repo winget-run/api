4fd47911f7e2965c7bc132f738cc8715d16070b3
Lukasz Niezabitowski <lukasznie1320@gmail.com>
2020-05-23 19:29:17 +0100

    fixes

--- src/server/plugins/ratelimit.ts
+++ src/server/plugins/ratelimit.ts
@@ -3,6 +3,6 @@ import { FastifyInstance } from "fastify";
 // TODO: implement
 export default async (fastify: FastifyInstance): Promise<void> => {
   fastify.addHook("onRequest", async () => {
-    fastify.log.info("ratelimit plugin called");
+    console.log("ratelimit plugin called");
   });
 };
