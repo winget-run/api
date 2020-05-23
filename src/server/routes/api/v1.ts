c25e5e6aba7da288743e83b2e2c4f50100059dd1
Ryan Larkin <rlarkin212@gmail.com>
2020-05-23 20:31:13 +0100

    work ot ill stab u

--- src/server/routes/api/v1.ts
+++ src/server/routes/api/v1.ts
@@ -5,6 +5,7 @@ import { PackageService } from "../../../database";
 
 import { request } from "http";
 import ghService from "../../ghService/index";
+import PackageModel from "../../../database/model/package";
 
 const DEFAULT_PAGE_SIZE = 12;
 const DEFAULT_AUTOCOMPLETE_SIZE = 3;
@@ -84,7 +85,7 @@ const orgPkgSchema = {
 
 export default async (fastify: FastifyInstance): Promise<void> => {
   // TODO: implement
-  fastify.setErrorHandler(async error => ({
+  fastify.setErrorHandler(async (error) => ({
     cunt: `oofie owie i made a fucky-(${error})-wucky`,
   }));
 
@@ -92,7 +93,7 @@ export default async (fastify: FastifyInstance): Promise<void> => {
     nonce: "yes",
   });
 
-  fastify.get("/search", { schema: searchSchema }, async request => {
+  fastify.get("/search", { schema: searchSchema }, async (request) => {
     const { query, page = 0 } = request.query;
 
     const pkgService = new PackageService();
@@ -110,39 +111,45 @@ export default async (fastify: FastifyInstance): Promise<void> => {
     };
   });
 
-  fastify.get("/autocomplete", { schema: autocompleteSchema }, async request => {
-    const { query } = request.query;
+  fastify.get(
+    "/autocomplete",
+    { schema: autocompleteSchema },
+    async (request) => {
+      const { query } = request.query;
 
-    const pkgService = new PackageService();
-    const packages = await Promise.all([
-      pkgService.find({
-        filters: {
-          Name: new RegExp(`.*${query}.*`),
-        },
-        take: DEFAULT_AUTOCOMPLETE_SIZE,
-      }),
-      pkgService.find({
-        filters: {
-          Publisher: new RegExp(`.*${query}.*`),
-        },
-        take: DEFAULT_AUTOCOMPLETE_SIZE,
-      }),
-      pkgService.find({
-        filters: {
-          Description: new RegExp(`.*${query}.*`),
-        },
-        take: DEFAULT_AUTOCOMPLETE_SIZE,
-      }),
-    ]).then((e) =>
-      e.flat().filter((f, i, a) => a.findIndex((g) => g.uuid === f.uuid) === i)
-    );
+      const pkgService = new PackageService();
+      const packages = await Promise.all([
+        pkgService.find({
+          filters: {
+            Name: new RegExp(`.*${query}.*`),
+          },
+          take: DEFAULT_AUTOCOMPLETE_SIZE,
+        }),
+        pkgService.find({
+          filters: {
+            Publisher: new RegExp(`.*${query}.*`),
+          },
+          take: DEFAULT_AUTOCOMPLETE_SIZE,
+        }),
+        pkgService.find({
+          filters: {
+            Description: new RegExp(`.*${query}.*`),
+          },
+          take: DEFAULT_AUTOCOMPLETE_SIZE,
+        }),
+      ]).then((e) =>
+        e
+          .flat()
+          .filter((f, i, a) => a.findIndex((g) => g.uuid === f.uuid) === i)
+      );
 
-    return {
-      packages,
-    };
-  });
+      return {
+        packages,
+      };
+    }
+  );
 
-  fastify.get("/:org", { schema: orgSchema }, async request => {
+  fastify.get("/:org", { schema: orgSchema }, async (request) => {
     const { org } = request.params;
     const { page = 0 } = request.query;
 
@@ -161,7 +168,7 @@ export default async (fastify: FastifyInstance): Promise<void> => {
     };
   });
 
-  fastify.get("/:org/:pkg", { schema: orgPkgSchema }, async request => {
+  fastify.get("/:org/:pkg", { schema: orgPkgSchema }, async (request) => {
     const { org, pkg } = request.params;
     const { page = 0 } = request.query;
 
@@ -183,7 +190,12 @@ export default async (fastify: FastifyInstance): Promise<void> => {
   //* import yaml endpoint
   fastify.get("/ghs/import", async (request) => {
     const yamls = await ghService.initialPackageImport();
+    const packageService = new PackageService();
 
-    return yamls;
+    const result = await Promise.all(
+      yamls.map((yaml) => packageService.insertOne(JSON.parse(yaml)))
+    );
+
+    return result;
   });
 };
