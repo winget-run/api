e899a35a79018cb154293881dd90a7afabd7e645
Lukasz Niezabitowski <lukasznie1320@gmail.com>
2020-05-23 22:03:12 +0100

    nonce

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,5 +3,9 @@ import { FastifyInstance } from "fastify";
 import api from "./api";
 
 export default async (fastify: FastifyInstance): Promise<void> => {
+  fastify.get("/", async () => ({
+    nonce: "rawrxd",
+  }));
+
   fastify.register(api, { prefix: "api" });
 };
