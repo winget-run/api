9055e4a2740f50507292767c4e54632e791074b3
Ryan Larkin <rlarkin212@gmail.com>
2020-06-14 15:38:13 +0100

    //wip base 64 helper for favicon

--- src/server/ghService/helpers/packageImageHelper.ts
+++ src/server/ghService/helpers/packageImageHelper.ts
@@ -1,14 +1,21 @@
 import { isNullOrUndefined } from "util";
+
 import fetch from "node-fetch";
+import { URL } from "url";
 
 const getFavicon = async (url: string): Promise<string> => {
   if (isNullOrUndefined(url)) {
     return "noURL";
   }
-  const favicon = await fetch(`${url}/favicon.ico`);
-  console.log(favicon);
 
-  return "yeet";
+  let { host } = new URL(url);
+  host = host.replace("www.", "http://");
+
+  const favicon = await fetch(`${host}/favicon.ico`)
+    .then(res => res.buffer())
+    .then(buf => buf.toString("base64"));
+
+  return favicon;
 };
 
 export = {
