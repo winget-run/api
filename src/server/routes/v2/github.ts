49579e71d521ab3982486b8d4cddf721147012a0
Ryan Larkin <rlarkin212@gmail.com>
2020-08-22 16:39:04 +0100

    formatting

--- src/server/routes/v2/github.ts
+++ src/server/routes/v2/github.ts
@@ -32,37 +32,33 @@ export default async (fastify: FastifyInstance): Promise<void> => {
   });
 
   // *----------------- manual package import---------------------
-  fastify.post(
-    "/manualImport",
-    { onRequest: validateApiToken },
-    async (req) => {
-      const manifests = req.body.manifests as string[];
-      const yamls = await ghService.manualPackageImport(manifests);
+  fastify.post("/manualImport", { onRequest: validateApiToken }, async (req) => {
+    const manifests = req.body.manifests as string[];
+    const yamls = await ghService.manualPackageImport(manifests);
 
-      console.log(`yamls length - ${yamls.length} - yamsl lemgth`);
+    console.log(`yamls length - ${yamls.length} - yamsl lemgth`);
 
-      const BATCH_SIZE = yamls.length % 2 === 0 ? 4 : 5;
-      let batchIndex = 0;
+    const BATCH_SIZE = yamls.length % 2 === 0 ? 4 : 5;
+    let batchIndex = 0;
 
-      while (batchIndex < yamls.length) {
-        const batch = yamls.slice(batchIndex, batchIndex + BATCH_SIZE);
+    while (batchIndex < yamls.length) {
+      const batch = yamls.slice(batchIndex, batchIndex + BATCH_SIZE);
 
-        for (let i = 0; i < batch.length; i += 1) {
-          const pkg = (batch[i] as unknown) as ManifestModel;
-          console.log(`${pkg.Id} / ${pkg.Version} - ${batchIndex}`);
+      for (let i = 0; i < batch.length; i += 1) {
+        const pkg = (batch[i] as unknown) as ManifestModel;
+        console.log(`${pkg.Id} / ${pkg.Version} - ${batchIndex}`);
 
-          // eslint- disable-next-line no-await-in-loop
-          // await addOrUpdatePackage(pkg);
-        }
-
-        batchIndex += BATCH_SIZE;
+        // eslint-disable-next-line no-await-in-loop
+        await addOrUpdatePackage(pkg);
       }
 
-      return {
-        Message: `imported ${
-          yamls.length
-        } packages at ${new Date().toISOString()}`,
-      };
-    },
-  );
+      batchIndex += BATCH_SIZE;
+    }
+
+    return {
+      Message: `imported ${
+        yamls.length
+      } packages at ${new Date().toISOString()}`,
+    };
+  });
 };
